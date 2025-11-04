d74d6fb7480bbea18325e1c196402fd3601de76f
Bailey Miles <baileyleah542@gmail.com>
2025-11-04 02:26:16 +0000

    Create base 2048 (needs logic)

---
+++ Scripts/games_selector.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_tactactoe_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_merge_pressed() -> void:
+	pass # Replace with function body.
+
+func _on__pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Games/2048.tscn")
